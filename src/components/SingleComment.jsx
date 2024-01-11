882a75b927c8807b84995980bfda2c0f5fedd760
Aucons94 <148054502+Aucons94@users.noreply.github.com>
2024-01-11 14:50:15 +0100

    Initial commit

---
+++ src/components/SingleComment.jsx
@@ -0,0 +1,33 @@
+import { Button, ListGroup } from "react-bootstrap";
+
+const SingleComment = ({ comment }) => {
+  const deleteComment = async (asin) => {
+    try {
+      let response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + asin, {
+        method: "DELETE",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTcxZDRhNTBkOGEyMDAwMThhNDhhNWEiLCJpYXQiOjE3MDQ5ODA1MzksImV4cCI6MTcwNjE5MDEzOX0.92UAo8XpTJ_SOz7UVjgANPXgZyusrfoKaQvFD3elz7k",
+        },
+      });
+      if (response.ok) {
+        alert("La recensione è stata elimata!");
+      } else {
+        throw new Error("La recensione non è stata eliminata!");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <ListGroup.Item>
+      {comment.comment}
+      <Button variant="danger" className="ms-2" onClick={() => deleteComment(comment._id)}>
+        Elimina
+      </Button>
+    </ListGroup.Item>
+  );
+};
+
+export default SingleComment;
